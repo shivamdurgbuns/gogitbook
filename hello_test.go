c0729b629fc8543590f28be9b1361fd706d669c8
shivam durgbuns <shivamdurgbuns@gmail.com>
2021-03-09 01:18:05 +0530

    Updated hello.go and hello_test with more test features.

--- hello_test.go
+++ hello_test.go
@@ -5,10 +5,26 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := hello("Shivam")
-	want := "Hello Shivam!"
 
-	if got != want {
-		t.Errorf("got: %q want: %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got: %q want: %q", got, want)
+		}
 	}
+
+	t.Run("Saying Hello to people.", func(t *testing.T) {
+		got := hello("Shivam")
+		want := "Hello, Shivam"
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("Saying Hello. World when empty string is supplied.", func(t *testing.T) {
+		got := hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+
+	})
+
 }
