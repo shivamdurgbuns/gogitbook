e153a090413199e5d01516e3e44a498f6a9d8686
shivam durgbuns <shivamdurgbuns@gmail.com>
2021-03-11 22:28:37 +0530

    Changed the array into slices in sum program

--- arrayAndSlices/sum.go
+++ arrayAndSlices/sum.go
@@ -3,10 +3,10 @@ package main
 /*
 Sum function takes array of numbers and return the sum of the numbers in the array
 */
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
--- arrayAndSlices/sum_test.go
+++ arrayAndSlices/sum_test.go
@@ -1,15 +1,32 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
-
-	got := Sum(numbers)
-	want := 15
-
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	assertCorrectMessage := func(t testing.TB, got int, want int, numbers []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
 	}
+
+	t.Run("Collection of 5 number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+		assertCorrectMessage(t, got, want, numbers)
+	})
+
+	t.Run("Collection of unknown numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+		assertCorrectMessage(t, got, want, numbers)
+	})
+
 }
