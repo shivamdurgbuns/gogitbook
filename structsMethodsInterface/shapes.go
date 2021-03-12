d19882ef535a21d2f1c07898bfaec7c8213af03a
shivam durgbuns <shivamdurgbuns@gmail.com>
2021-03-12 23:36:31 +0530

    Added area function and its test case to shapes.go program

--- structsMethodsInterface/shapes.go
+++ structsMethodsInterface/shapes.go
@@ -6,3 +6,10 @@ Perimeter function returns the perimeter of a square/rectangle.
 func Perimeter(width float64, height float64) float64 {
 	return 2 * (width + height)
 }
+
+/*
+Area functions gives the area of a square/rectangel
+*/
+func Area(width float64, height float64) float64 {
+	return width * height
+}
