2e679ee0ccf20b83d29ddff5955113cd7db6a08e
shivam durgbuns <shivamdurgbuns@gmail.com>
2021-03-14 17:08:43 +0530

    added noerrors method in wallet_test.go

--- pointersAndErrors/wallet_test.go
+++ pointersAndErrors/wallet_test.go
@@ -6,45 +6,53 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	assertBalance := func(t testing.TB, wallet Wallet, want Shivcoin) {
-		t.Helper()
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t testing.TB, got error, want error) {
-		t.Helper()
-		if got == nil {
-			t.Fatal("Didnt got any errors")
-		}
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	}
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
-
 		wallet.Deposit(Shivcoin(10))
+
 		assertBalance(t, wallet, Shivcoin(10))
 	})
 
-	t.Run("Withdraw", func(t *testing.T) {
-		wallet := Wallet{balance: Shivcoin(20)}
+	t.Run("Withdraw with funds", func(t *testing.T) {
+		wallet := Wallet{Shivcoin(20)}
+		err := wallet.Withdraw(Shivcoin(10))
 
-		wallet.Withdraw(Shivcoin(10))
 		assertBalance(t, wallet, Shivcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
-		startingBalance := Shivcoin(20)
-		wallet := Wallet{startingBalance}
+		wallet := Wallet{Shivcoin(20)}
 		err := wallet.Withdraw(Shivcoin(100))
 
-		assertBalance(t, wallet, startingBalance)
+		assertBalance(t, wallet, Shivcoin(20))
 		assertError(t, err, ErrInsufficientFunds)
 	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Shivcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
 }
